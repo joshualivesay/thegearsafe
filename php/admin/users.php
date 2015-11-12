e89530d8cc2d5fdf554aa4a77c20d5084be70c81
JoshuaLivesay <joshualivesay@gmail.com>
2015-11-12 12:39:54 -0500

    Added new user setting

--- php/admin/users.php
+++ php/admin/users.php
@@ -1,7 +1,8 @@
 <?php
 	$users = array(
-		"elflaire" => "04fdf4f93e6b60fc7acd94f47028ce7f", 
-		"Envato" => "4a3f8e0dc6f175f2ff0e069904012453" 
+		"elflaire" => "i426f9d58cecb209c385c04a61d4d60b7", 
+		"Envato" => "426f9d58cecb209c385c04a61d4d60b7",
+		"jlivesay" => "426f9d58cecb209c385c04a61d4d60b7" 
 	);
 	$salt = substr(md5(date("F")), 8);
 	$displayName = "Admin";
